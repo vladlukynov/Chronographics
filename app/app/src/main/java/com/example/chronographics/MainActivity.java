5417706d295105898ad5e23e329df1fe5b822559
vladlukynov <lukynov.1@yandex.ru>
2023-01-11 02:51:44 +0300

    Add empty home page

--- app/app/src/main/java/com/example/chronographics/MainActivity.java
+++ app/app/src/main/java/com/example/chronographics/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.chronographics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.main_activity_container);
+        if (fragment == null) {
+            fragment = new AllDoneFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_activity_container, fragment)
+                    .commit();
+        }
     }
 }
